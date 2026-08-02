439f191e0b09172b1d0505ed2b64282c6af4957b
凉数中 <2791389901@qq.com>
2026-08-02 22:23:02 +0800

    创建 AuthProvider.tsx

---
+++ src/components/AuthProvider.tsx
@@ -0,0 +1,14 @@
+import { AuthUIProvider } from "@daveyplate/better-auth-ui";
+import { Toaster } from "sonner";
+import { authClient } from "@/lib/auth-client";
+
+export function AuthProvider({ children }: { children: React.ReactNode }) {
+  return (
+    <>
+      <Toaster />
+      <AuthUIProvider authClient={authClient}>
+        {children}
+      </AuthUIProvider>
+    </>
+  );
+}
